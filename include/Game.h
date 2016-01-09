dc4a9a4710a0d94e16ee744a88bb245bafdaca02
Gaulois94 <serenomickael@gmail.com>
2016-01-09 17:23:49 +0100

    Change Game structure by creating Window structure

--- include/Game.h
+++ include/Game.h
@@ -3,17 +3,17 @@
 
 #include "stdlib.h"
 #include "Context.h"
+#include "globalVar.h"
 #include "SDL2/SDL.h"
 
 typedef struct
 {
 	Context* currentContext;
+	SDL_Event* event;
 }Game;
 
 Game* Game_new();
 void  Game_run(Game* game);
-int   Game_initVideo(Game* game);
-void  Game_fpsManager(Game* game);
 void  Game_destroy();
 
 #endif
