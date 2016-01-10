1283f3e10c73acb3787c8d6ef7a4d9bd108df652
Gaulois94 <serenomickael@gmail.com>
2016-01-10 15:39:45 +0100

    Add the object List

---
+++ include/List.h
@@ -0,0 +1,26 @@
+#ifndef  LIST_INC
+#define  LIST_INC
+
+typedef char bool;
+
+typedef struct ListElem
+{
+	ListElem* next;
+	void* data;
+}ListElem;
+
+typedef struct
+{
+	ListElem* head;
+	uint32_t len;
+}List;
+
+List*    List_createList();
+void     List_addData(List* self, void* data);
+uint32_t List_getLen(List* self);
+void*    List_getData(List* self, uint32_t id);
+void     List_deleteData(List* self, void* data);
+void     List_clear(List* self, bool freeDatas);
+void     List_destroy(List* self, bool freeDatas);
+
+#endif
