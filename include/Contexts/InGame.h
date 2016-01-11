335e7334d4579f59c0adb8923d2a9c872af3e5ab
Gaulois94 <serenomickael@gmail.com>
2016-01-11 18:25:48 +0000

    Start thinking about InGame context

--- include/Contexts/InGame.h
+++ include/Contexts/InGame.h
@@ -1,15 +1,29 @@
 #ifndef  INGAME_INC
 #define  INGAME_INC
 
-#include "Context.h"
+#include "Contexts/Context.h"
+#include "Ennemy.h"
+#include "Player.h"
 
 typedef struct
 {
 	Context  base;
+
 	Ennemy** ennemies;
 	uint32_t nbEnnemies;
 
+	Player*  player;
+
 	Map*     map;
 }InGame
+
+InGame*     InGame_create();
+void        InGame_init(InGame* self);
+EnumContext InGame_run(Context* self);
+void        InGame_updateEnnemies(InGame* self);
+void        InGame_updatePlayer(InGame* self);
+void        InGame_loadMap(InGame* self, const char* path);
+void        InGame_destroy(InGame* self);
+
 
 #endif
---
+++ src/Contexts/InGame.c
@@ -0,0 +1,72 @@
+#include "Contexts/InGame.h"
+
+InGame* InGame_create()
+{
+	InGame* self = (InGame*)malloc(sizeof(InGame));
+	if(self == NULL)
+	{
+		perror("Error in malloc");
+		return NULL;
+	}
+	return self;
+}
+
+void InGame_init(InGame* self)
+{
+	self->map        = NULL;
+	self->ennemies   = NULL;
+	self->nbEnnemies = 0;
+
+	self->player     = Player_create();
+
+	((Context*)self)->run = &InGame_run;
+}
+
+EnumContext InGame_run(Context* context)
+{
+	InGame* self = (InGame*)context;
+	if(self->map == NULL || globalVar_window)
+		return;
+
+	//We first update our datas
+	InGame_updateEnnemies(self);
+	InGame_updatePlayer(self);
+
+	//Then we display them
+	Map_draw(self->map);
+	uint32_t i;
+	for(i=0; i < self->nbEnnemies; i++)
+		self->ennemies[i]->draw(self->ennemies[i], globalVar_window->renderer);
+
+	self->player->draw(self->player, globalVar_window->renderer);
+}
+
+void InGame_updateEnnemies(InGame* self)
+{
+
+}
+
+void InGame_updatePlayer(InGame* self)
+{
+
+}
+
+void InGame_loadMap(InGame* self, const char* path)
+{
+	if(self->map)
+		Map_destroy(self->map);
+
+	self->map = NULL;
+	self->map = Map_create(path);
+}
+
+void InGame_destroy(InGame* self)
+{
+	uint32_t i;
+	for(i=0; i < self->nbEnnemies; i++)
+		Ennemy_destroy(self->ennemies[i]);
+	if(self->map)
+		Map_destroy(self->map);
+	if(self->player)
+		Player_destroy(self->player);
+}
