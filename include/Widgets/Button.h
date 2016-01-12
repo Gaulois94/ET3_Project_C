cb9431c4cadc4f67fe4626a29d40cd738f23a7dd
Gaulois94 <serenomickael@gmail.com>
2016-01-12 19:37:03 +0100

    First usable Button version

--- include/Widgets/Button.h
+++ include/Widgets/Button.h
@@ -13,9 +13,11 @@ typedef struct
 
 Button* Button_create(void(*callback)(void*), void* data, const SDL_Rect* rect, Sprite* image, Text* text);
 void Button_init(Button* self, void(*callback)(void*), void* data, const SDL_Rect* rect, Sprite* image, Text* text);
-void Button_howActive(Active* self, const SDL_Event* event);
-void Button_howDisactive(Active* self, const SDL_Event* event);
-void Button_draw(Drawable* drawable, SDL_Renderer* renderer);
+void Button_updateDrawableRect(Button* self);
+bool Button_howActive(Active* self, const SDL_Event* event);
+void Button_draw(Drawable* drawable, Window* window);
+void Button_setSize(Drawable* drawable, uint32_t width, uint32_t height);
+void Button_setPosition(Drawable* drawable, int32_t x, int32_t y);
 void Button_destroy(Button* self);
 
 #endif
--- src/Widgets/Button.c
+++ src/Widgets/Button.c
@@ -9,30 +9,73 @@ Button* Button_create(void(*callback)(void*), void* data, const SDL_Rect* rect, 
 		return NULL;
 	}
 	Button_init(button, callback, data, rect, image, text);
+	return button;
 }
 
 void Button_init(Button* self, void(*callback)(void*), void* data, const SDL_Rect* rect, Sprite* image, Text* text)
 {
+	Drawable_init((Drawable*)self, rect);
+	((Drawable*)self)->setPosition = &Button_setPosition;
+	((Drawable*)self)->setSize = &Button_setSize;
+	((Active*)self)->howActive = &Button_howActive;
+	self->sprite = image;
+	self->text = text;
 
+	Button_updateDrawableRect(self);
 }
 
-void Button_howActive(Active* self, const SDL_Event* event)
+bool Button_howActive(Active* self, const SDL_Event* event)
 {
-
+	if(event->type == SDL_MOUSEBUTTONDOWN)
+	{
+		SDL_Point p;
+		p.x = event->button.x;
+		p.y = event->button.y;
+		if(Drawable_pointOnRect((Drawable*)self, &p))
+			return true;
+	}
+	return false;
 }
 
-void Button_howDisactive(Active* self, const SDL_Event* event)
+void Button_updateDrawableRect(Button* self)
 {
+	const SDL_Rect* rect = Drawable_getRect((Drawable*)self);
+	if(self->sprite)
+		Drawable_setRect(((Drawable*)self->sprite), rect);
+	if(self->text)
+	{
+		const SDL_Rect* textRect = Drawable_getRect((Drawable*)self->text);
+		float scale = 0.9 * fmin(rect->x / (float)textRect->x, rect->y / (float)textRect->y);
+		Drawable_setSize(((Drawable*)self->text), textRect->x * scale, textRect->y * scale);
 
+		int32_t buttonCenterX = rect->x + rect->w / 2;
+		int32_t buttonCenterY = rect->y + rect->h / 2;
+		textRect = Drawable_getRect((Drawable*)self->text);
+		int32_t textCenterX = textRect->x + textRect->w / 2;
+		int32_t textCenterY = textRect->y + textRect->h / 2;
+		Drawable_setPosition((Drawable*)self->text, buttonCenterX-textCenterX, buttonCenterY-textCenterY);
+	}
 }
 
-void Button_draw(Drawable* drawable, SDL_Renderer* renderer)
+void Button_setPosition(Drawable* drawable, int32_t x, int32_t y)
+{
+	Drawable_setPosition(drawable, x, y);
+	Button_updateDrawableRect((Button*)drawable);
+}
+
+void Button_setSize(Drawable* drawable, uint32_t w, uint32_t h)
+{
+	Drawable_setSize(drawable, w, h);
+	Button_updateDrawableRect((Button*)drawable);
+}
+
+void Button_draw(Drawable* drawable, Window* window)
 {
 	Button* self = (Button*)drawable;
 	if(self->sprite)
-		Sprite_draw(((Drawable*)self->sprite), renderer);
+		Sprite_draw(((Drawable*)self->sprite), window);
 	if(self->text)
-		Text_draw(((Drawable*)self->text), renderer);
+		Text_draw(((Drawable*)self->text), window);
 }
 
 void Button_destroy(Button* self)
