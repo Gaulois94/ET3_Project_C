88c1089db05990f379def085f54e8eec7ed04786
Gaulois94 <serenomickael@gmail.com>
2016-01-12 19:37:27 +0100

    Add some rect functions useful

--- include/Drawable/Drawable.h
+++ include/Drawable/Drawable.h
@@ -4,6 +4,7 @@
 #include "SDL2/SDL.h"
 #include "Widgets/Active.h"
 #include "typedefine.h"
+#include "Window.h"
 
 /* \struct Drawable
  * \brief Drawing things on the screen. Don't use this structure directly : use another which inherit from it.*/
@@ -11,19 +12,29 @@ typedef struct Drawable
 {
 	Active base;
 	SDL_Rect rect; /*!< Destination rect.*/
-	void(*draw)(struct Drawable*, SDL_Renderer*); /* !<Function which point to the correct draw function (use for inheritance) */
+	void(*draw)(struct Drawable*, Window*); /* !<Function which point to the correct draw function (use for inheritance) */
 	void(*destroy)(struct Drawable*);
+	void(*setStatic)(struct Drawable*, bool);
+	void(*setPosition)(struct Drawable*, int32_t, int32_t);
+	void(*setSize)(struct Drawable*, uint32_t width, uint32_t height);
+	bool isStatic;
 }Drawable;
 
 void Drawable_init(Drawable* self, const SDL_Rect* destRect);
-void Drawable_draw(Drawable* self, SDL_Renderer* renderer); //This function does nothing, but is necessary for the pointer Drawable::draw. We keep the SDL_Renderer as a parameter, even if globalVar_window exist, for the modularity of the code. Indeed, the class Drawable and others class like this one could be reused in other projects.
-const SDL_Rect* Drawable_getRect(Drawable* self);
+void Drawable_draw(Drawable* self, Window* window); //This function does nothing, but is necessary for the pointer Drawable::draw. We keep the Window as a parameter, even if globalVar_window exist, for the modularity of the code. Indeed, the class Drawable and others class like this one could be reused in other projects.
+const SDL_Rect* Drawable_getRect(const Drawable* self);
+void Drawable_setRect(Drawable* self, const SDL_Rect* rect);
+SDL_Rect Drawable_getRectOnScreen(const Drawable* self, const Window* window);
+bool Drawable_pointOnRect(const Drawable* drawable, const SDL_Point* p);
+bool Drawable_pointOnRectOnScreen(const Drawable* self, const Window* window, const SDL_Point* p);
 void Drawable_setSize(Drawable* self, uint32_t width, uint32_t height);
 void Drawable_setPosition(Drawable* self, int32_t x, int32_t y);
+void Drawable_setStatic(Drawable* self, bool isStatic);
 
 void copyRect(SDL_Rect* dest, const SDL_Rect* src);
 const SDL_Rect* evaluateRect(const SDL_Rect* rect);
 
 void Drawable_destroy(Drawable* self);
+bool pointOnRect(const SDL_Rect* rect, const SDL_Point* p);
 
 #endif
--- src/Drawable/Drawable.c
+++ src/Drawable/Drawable.c
@@ -5,18 +5,51 @@ void Drawable_init(Drawable* self, const SDL_Rect* destRect)
 	Active_init((Active*)self);
 	self->draw = NULL;
 	self->destroy = &Drawable_destroy;
+	self->isStatic = false;
+	self->setStatic = &Drawable_setStatic;
+	self->setPosition = &Drawable_setPosition;
+	self->setSize = &Drawable_setSize;
 	copyRect(&(self->rect), destRect);
 }
 
-void Drawable_draw(Drawable* self, SDL_Renderer* renderer)
+void Drawable_draw(Drawable* self, Window* window)
 {
 	if(self->draw)
-		self->draw(self, renderer);
+		self->draw(self, window);
 }
 
-const SDL_Rect* Drawable_getRect(Drawable* self)
+const SDL_Rect* Drawable_getRect(const Drawable* self)
 {
 	return &(self->rect);
+}
+
+bool Drawable_pointOnRect(const Drawable* self, const SDL_Point* p)
+{
+	return pointOnRect(&(self->rect), p);
+}
+
+bool Drawable_pointOnRectOnScreen(const Drawable* self, const Window* window, const SDL_Point* p)
+{
+
+	SDL_Rect rect = Drawable_getRectOnScreen(self, window);
+	return pointOnRect(&rect, p);
+}
+
+SDL_Rect Drawable_getRectOnScreen(const Drawable* self, const Window* window)
+{
+	if(self->isStatic)
+		return self->rect;
+	SDL_Point p;
+	SDL_Rect rect;
+	rect.w = self->rect.w;
+	rect.h = self->rect.h;
+	p.x = self->rect.x;
+	p.y = self->rect.y;
+
+	p = Window_convertToScreenCoord(window, &p);
+	rect.x = p.x;
+	rect.y = p.y;
+	return rect;
 }
 
 void Drawable_setSize(Drawable* self, uint32_t width, uint32_t height)
@@ -29,6 +62,17 @@ void Drawable_setPosition(Drawable* self, int32_t x, int32_t y)
 {
 	self->rect.x = x;
 	self->rect.y = y;
+}
+
+void Drawable_setStatic(Drawable* self, bool isStatic)
+{
+	self->isStatic = isStatic;
+}
+
+void Drawable_setRect(Drawable* self, const SDL_Rect* rect)
+{
+	self->setPosition(self, rect->x, rect->y);
+	self->setSize(self, rect->x, rect->y);
 }
 
 void copyRect(SDL_Rect* dest, const SDL_Rect* src)
@@ -55,4 +99,10 @@ const SDL_Rect* evaluateRect(const SDL_Rect* rect)
 void Drawable_destroy(Drawable* self)
 {
 	free(self);
+}
+
+bool pointOnRect(const SDL_Rect* rect, const SDL_Point* p)
+{
+	return (p->x >= rect->x && p->x <= rect->x + rect->w &&
+			p->y >= rect->y && p->y <= rect->y + rect->h);
 }
