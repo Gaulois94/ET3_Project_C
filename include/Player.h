995f79db2294631fe270add96953be395b5c65a3
Gaulois94 <serenomickael@gmail.com>
2016-01-13 21:22:12 +0100

    First version of Player

--- include/Player.h
+++ include/Player.h
@@ -3,16 +3,20 @@
 
 #include "stdint.h"
 #include "Drawable/StaticAnimation.h"
+#include "File.h"
 
 #define PLAYER_SPEED_X 800
 #define PLAYER_SPEED_Y 100
 
-typedef enum{TOP, BOTTOM, LEFT, RIGHT} Orientation;
+typedef enum{RIGHT, LEFT, TOP, BOTTOM} Orientation;
 
 typedef struct
 {
 	Drawable base;
-	StaticAnimation* staticAnimation;
+	File* playerImage;
+	StaticAnimation** staticAnimation;
+	uint8_t    animLength;
+	uint8_t    idAnimation;
 	uint8_t    lifes;
 	Orientation orientation;
 }Player;
@@ -21,6 +25,10 @@ Player* Player_create(int32_t x, int32_t y);
 void    Player_init(Player* player, int32_t x, int32_t y);
 bool    Player_howActive(Active* active, SDL_Event* e);
 void    Player_activeIt(Active* active);
-void    Player_destroy(Player* player);
+void    Player_draw(Drawable* drawable, Window* window);
+void    Player_setPosition(Drawable* drawable, int32_t x, int32_t y);
+void    Player_setStatic(Drawable* drawable, bool isStatic);
+void    Player_setSize(Drawable* drawable, uint32_t width, uint32_t height);
+void    Player_destroy(Drawable* drawable);
 
 #endif
--- src/Player.c
+++ src/Player.c
@@ -19,8 +19,38 @@ void Player_init(Player* self, int32_t x, int32_t y)
 	Drawable_init((Drawable*)self, &rect);
 
 	self->lifes = 3;
-	self->staticAnimation = NULL;
+	self->animLength = 1;
+
+	self->playerImage = File_create("Resources/player.png");
+	if(self->playerImage == NULL)
+	{
+		perror("Couldn't load the player image file. abort");
+		return;
+	}
+
+	int32_t i;
+	for(i=0; i < self->animLength; i++)
+	{
+		self->staticAnimation = (StaticAnimation**)malloc(sizeof(StaticAnimation*)*self->animLength);
+		self->staticAnimation[0] = StaticAnimation_create(NULL, self->playerImage->texture, 0, i*32, 0, 0, 29, 32, 3, 3, 0, 8);
+	}
+
+	self->idAnimation = 0;
 	self->orientation=RIGHT;
+	Player_setPosition((Drawable*)self, x, y);
+
+	Drawable* selfDrawable = (Drawable*)self;
+	selfDrawable->setPosition = &Player_setPosition;
+	selfDrawable->setSize     = &Player_setSize;
+	selfDrawable->setStatic   = &Player_setStatic;
+	selfDrawable->destroy     = &Player_destroy;
+
+}
+
+void Player_draw(Drawable* drawable, Window* window)
+{
+	Player* self = (Player*)drawable;
+	((Drawable*)(self->staticAnimation[self->idAnimation]))->draw((Drawable*)(self->staticAnimation[self->idAnimation]), window);
 }
 
 bool Player_howActive(Active* active, SDL_Event* e)
@@ -52,7 +82,41 @@ void Player_activeIt(Active* active)
 	}
 }
 
-void Player_destroy(Player* player)
+void Player_setPosition(Drawable* drawable, int32_t x, int32_t y)
 {
-	free(player);
+	Player* self = (Player*)drawable;
+	int32_t i;
+	for(i=0; i < self->animLength; i++)
+		((Drawable*)(self->staticAnimation[i]))->setPosition((Drawable*)(self->staticAnimation[i]), x, y);
+	Drawable_setPosition(drawable, x, y);
+}
+
+void Player_setSize(Drawable* drawable, uint32_t width, uint32_t height)
+{
+	Player* self = (Player*)drawable;
+	int32_t i;
+	for(i=0; i < self->animLength; i++)
+		((Drawable*)(self->staticAnimation[i]))->setSize((Drawable*)(self->staticAnimation[i]), width, height);
+	Drawable_setSize(drawable, width, height);
+}
+
+void Player_setStatic(Drawable* drawable, bool isStatic)
+{
+	Player* self = (Player*)drawable;
+	int32_t i;
+	for(i=0; i < self->animLength; i++)
+		((Drawable*)(self->staticAnimation[i]))->setStatic((Drawable*)(self->staticAnimation[i]), isStatic);
+	Drawable_setStatic(drawable, isStatic);
+}
+
+void Player_destroy(Drawable* drawable)
+{
+	Player* self = (Player*)drawable;
+	int8_t i;
+	for(i=0; i < self->animLength; i++)
+		if(self->staticAnimation[i])
+			((Drawable*)(self->staticAnimation[i]))->destroy((Drawable*)(self->staticAnimation[i]));
+	File_destroy(self->playerImage);
+	free(self->staticAnimation);
+	free(self);
 }
