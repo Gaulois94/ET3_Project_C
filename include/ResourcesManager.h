4f2dd354f7e33231890ff0c34bb3966fd103c567
Gaulois94 <serenomickael@gmail.com>
2016-01-13 12:22:54 +0000

    Add documentation

--- include/ResourcesManager.h
+++ include/ResourcesManager.h
@@ -12,12 +12,54 @@ typedef struct
 	uint32_t       len;
 }ResourcesManager;
 
-ResourcesManager* ResourcesManager_init();
+/** \brief init the resourcesManager
+ * \return return the new ResourcesManager*/
+ResourcesManager* ResourcesManager_create();
+
+/** \brief return the data associate with the key
+ * \param resources the ResourcesManager
+ * \param key the key associate to the data
+ * \return the data*/
 void*             ResourcesManager_getData(ResourcesManager* resources, const char* key);
+
+/** \brief return the key associate to the data
+ * \param resources the ResourcesManager
+ * \param data the pointer to data
+ * \return the key*/
 const char*       ResourcesManager_getKey(const ResourcesManager* resources, const void* data);
+
+/** \brief return the length of the array ResourcesManager
+ * \param resources the ResourcesManager
+ * \return the len*/
 uint32_t          ResourcesManager_getLen(const ResourcesManager* resources);
-void              ResourcesManager_addData(ResourcesManager* resources, const char* key, void* data);
-bool              ResourcesManager_deleteData(ResourcesManager* resources, const char* key);
+
+/** \brief get the data by its ID
+ * \param resources the ResourcesManager
+ * \param i the ID
+ * \return the data*/
+void*             ResourcesManager_getDataByID(ResourcesManager, uint32_t i);
+
+/** \brief get the key by its ID
+ * \param resources the ResourcesManager
+ * \param i the ID
+ * \return the key*/
+char*             ResourcesManager_getKeyByID(ResourcesManager, uint32_t i);
+
+/** \brief add a data to the ResourcesManager
+ * \param resources the ResourcesManager
+ * \param key the key which will be associate to the data
+ * \param data the data
+ * \return if the data table could be added (the key doesn't exist)*/
+bool              ResourcesManager_addData(ResourcesManager* resources, const char* key, void* data);
+
+/** \brief remove a data on the ResourcesManager associate with its key
+ * \param resources the ResourcesManager
+ * \param key the key
+ * \return the data removed*/
+void*             ResourcesManager_removeData(ResourcesManager* resources, const char* key);
+
+/** \brief remove the ResourcesManager
+ * \param resources the ResourcesManager*/
 void              ResourcesManager_destroy(ResourcesManager* resources);
 
 #endif
