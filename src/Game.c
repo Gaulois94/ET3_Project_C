40ad973a5e2984d28f4b2639738527e3694fe592
Gaulois94 <serenomickael@gmail.com>
2016-01-13 19:07:48 +0000

    Add main loop

--- src/Game.c
+++ src/Game.c
@@ -4,6 +4,7 @@ Game* Game_create()
 {
 	Game* game = (Game*)malloc(sizeof(Game));
 	game->currentContext = NULL;
+	game->quit = false;
 	globalVar_window = Window_init(SCREEN_WIDTH, SCREEN_HEIGHT, "Mario Like");
 
 	return game;
@@ -11,6 +12,21 @@ Game* Game_create()
 
 void Game_run(Game* game)
 {
+	while(!game->quit)
+	{
+		while(SDL_PollEvent(&(game->event)))
+		{
+			if(game->event.type == SDL_QUIT)
+			{
+				game->quit = true;
+				break;
+			}
+		}
+		if(!game->quit)
+		{
+
+		}
+	}
 }
 
 void Game_destroy(Game* game)
