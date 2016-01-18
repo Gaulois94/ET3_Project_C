7ee3c2aa553fff17ddd181cdf30b609c6baafdfd
Gaulois94 <serenomickael@gmail.com>
2016-01-18 20:33:39 +0000

    Tile for the start of the map

---
+++ include/Tiles/TileStart.h
@@ -0,0 +1,13 @@
+#ifndef  TILESTART_INC
+#define  TILESTART_INC
+
+#include "Tiles/Tile.h"
+
+typedef struct
+{
+
+}TileStart;
+
+Tile* TileStart_create(SDL_Texture* texture, const SDL_Rect* subRect);
+
+#endif
